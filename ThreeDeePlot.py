9c21d6227cb584bcaa4f1768d9b63a58ce67af67
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-01-31 15:42:32 -0500

    3D PLOT DONE!

--- ThreeDeePlot.py
+++ ThreeDeePlot.py
@@ -16,23 +16,23 @@ ax = fig.add_subplot(111, projection='3d')
 with open('2003.csv') as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
-        Team.append(int(row[0]))
-        OPR.append(int(row[1]))
-        DPR.append(int(row[2]))
-        CCWM.append(int(row[2]))
+        Team.append(row[0])
+        OPR.append(float(row[1]))
+        DPR.append(float(row[2]))
+        CCWM.append(float(row[3]))
 
 
-#x = [int(i) for i in OPR]
-#y = [int(i) for i in DPR]
-#z = [int(i) for i in CCWM]
+    x = [int(i) for i in OPR]
+    y = [int(i) for i in DPR]
+    z = [int(i) for i in CCWM]
 
-ax.scatter(x, y, z, c='r', marker='o')
+    ax.scatter(x, y, z, c='r', marker='o')
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
-plt.show()
+    ax.set_xlabel('OPR')
+    ax.set_ylabel('DPR')
+    ax.set_zlabel('CCWM')
+    plt.show()
 
-
-#Need to turn list into integers/float to be able to plot them. The first possible issue could be the commas and ' ' marks, which we can remove. Otherwise, storing in a list may
-#(cont) not be the best idea. 
+#print(OPR)
+    #Need to turn list into integers/float to be able to plot them. The first possible issue could be the commas and ' ' marks, which we can remove. Otherwise, storing in a list may
+    #(cont) not be the best idea. 
