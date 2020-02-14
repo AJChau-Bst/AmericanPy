d8030b7ebfe3b611f3c4d2e657a45126a20ddb97
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-02-13 19:54:14 -0500

    im going to revert this commitment

---
+++ tkintertest.py
@@ -0,0 +1,11 @@
+import tkinter
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)               
+        self.master = master
+        root = Tk()
+        app = Window(root)
+
+        root.mainloop()
+        
